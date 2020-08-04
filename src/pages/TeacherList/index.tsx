7a573615b469df5ace4fd20fe11b3b08003dea2f
Amanda Silva <amanda-ms@hotmail.com>
2020-08-03 23:03:28 -0300

    componente page header

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import logoImg from '../../assets/images/logo.svg'
-import backIcon from '../../assets/images/icons/back.svg'
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
 
 function TeacherList(){
   return(
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Boltar"/>
-          </Link>
-          <img src={logoImg} alt="Proffy"/>
-        </div>
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
 
-        <div className="header-content">
-          <strong>Estes são os proffys disponíveis.</strong>
-        </div>
-      </header>
+          <div className="input-block">
+            <label htmlFor="week-day">Dias da semana</label>
+            <input type="text" id="week-day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
     </div>
   )
 }
